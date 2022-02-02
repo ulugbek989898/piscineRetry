149533e373f7b22b990e6575df919851e06db505
Ulugbek Isroilov <uisroilo@lab3r1s12.42abudhabi.ae>
2022-02-02 09:45:34 +0400

    argc, argv is done c06

---
+++ c06/ex01/ft_print_params.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: uisroilo <uisroilo@student.42abudhabi.ae>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/02 09:15:50 by uisroilo          #+#    #+#             */
+/*   Updated: 2022/02/02 09:32:22 by uisroilo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	if (argc > 1)
+	{
+		i = 1;
+		while (i < argc)
+		{
+			j = 0;
+			while (argv[i][j])
+				j++;
+			write(1, argv[i], j);
+			ft_putchar('\n');
+			i++;
+		}
+	}
+}
---
+++ c06/ex02/ft_rev_params.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: uisroilo <uisroilo@student.42abudhabi.ae>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/02 09:33:19 by uisroilo          #+#    #+#             */
+/*   Updated: 2022/02/02 09:40:40 by uisroilo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	if (argc > 1)
+	{
+		i = argc - 1;
+		while (i > 0)
+		{
+			j = 0;
+			while (argv[i][j])
+				j++;
+			write(1, argv[i], j);
+			ft_putchar('\n');
+			i--;
+		}
+	}
+	return (0);
+}
